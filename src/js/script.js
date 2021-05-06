efef34e14287490df5da791d925802ec9b333f39
Marta Wojdat <marjow4@gmail.com>
2021-05-06 23:47:44 +0200

    Add const for selected options

--- src/js/script.js
+++ src/js/script.js
@@ -152,7 +152,9 @@
           // determine option value, e.g. optionId = 'olives', option = { label: 'Olives', price: 2, default: true }
           const option = param.options[optionId];
           // check if there is param with a name of paramId in formData and if it includes optionId
-          if(formData[paramId] && formData[paramId].includes(optionId)) {
+          const optionSelected = formData[paramId] && formData[paramId].includes(optionId);
+
+          if(optionSelected) {
             // check if the option is not default
             if(option.hasOwnProperty('default') == false) {
               // add option price to price variable
@@ -169,7 +171,7 @@
           const optionImage = thisProduct.imageWrapper.querySelector('.' + paramId + '-' + optionId);
           
           if(optionImage != null) {
-            if(formData[paramId] && formData[paramId].includes(optionId)) {
+            if(optionSelected) {
               optionImage.classList.add(classNames.menuProduct.imageVisible);
             } else {
               optionImage.classList.remove(classNames.menuProduct.imageVisible);
