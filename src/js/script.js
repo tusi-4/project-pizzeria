d6df9375081976a0e7e5853354d3120e44d11bf5
Marta Wojdat <marjow4@gmail.com>
2021-05-06 21:55:57 +0200

    Add images

--- src/js/script.js
+++ src/js/script.js
@@ -39,14 +39,14 @@
       imageVisible: 'active',
     },
   };
-
+  /* musze zakomentowac, bo test:js sie czepia
   const settings = {
     amountWidget: {
       defaultValue: 1,
       defaultMin: 1,
       defaultMax: 9,
     } 
-  };
+  }; */
 
   const templates = {
     menuProduct: Handlebars.compile(document.querySelector(select.templateOf.menuProduct).innerHTML),
@@ -90,6 +90,7 @@
       thisProduct.formInputs = thisProduct.form.querySelectorAll(select.all.formInputs);
       thisProduct.cartButton = thisProduct.element.querySelector(select.menuProduct.cartButton);
       thisProduct.priceElem = thisProduct.element.querySelector(select.menuProduct.priceElem);
+      thisProduct.imageWrapper = thisProduct.element.querySelector(select.menuProduct.imageWrapper);
     }
 
     initAccordion(){
@@ -162,6 +163,16 @@
             if(option.default == true) {
               // reduce price variable
               price -= option.price;
+            }
+          }
+          // images
+          const optionImage = thisProduct.imageWrapper.querySelector('.' + paramId + '-' + optionId);
+          
+          if(optionImage != null) {
+            if(formData[paramId] && formData[paramId].includes(optionId)) {
+              optionImage.classList.add(classNames.menuProduct.imageVisible);
+            } else {
+              optionImage.classList.remove(classNames.menuProduct.imageVisible);
             }
           }
         }
