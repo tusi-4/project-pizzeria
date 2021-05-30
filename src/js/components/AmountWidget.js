4c1f86233ee21e120744281579e91c3e9b990f6e
Marta Wojdat <marjow4@gmail.com>
2021-05-30 14:09:29 +0200

    Add date and hour pickers

--- src/js/components/AmountWidget.js
+++ src/js/components/AmountWidget.js
@@ -1,51 +1,53 @@
 import {settings, select} from '../settings.js';
+import BaseWidget from './BaseWidget.js';
 
-class AmountWidget{
+class AmountWidget extends BaseWidget{
   constructor(element){
+    super(element, settings.amountWidget.defaultValue);
+
     const thisWidget = this;
 
     thisWidget.getElements(element);
-    thisWidget.setValue(settings.amountWidget.defaultValue);
+    thisWidget.setValue(); // dodalam do konstruktora zainspirowana forum M-GT & PW i w widgecie wartosc jest dobra, w koszyku znowu zla
     thisWidget.initActions();
   }
 
-  getElements(element){
+  getElements(){
     const thisWidget = this;
 
-    thisWidget.element = element;
-    thisWidget.input = thisWidget.element.querySelector(select.widgets.amount.input);
-    thisWidget.linkDecrease = thisWidget.element.querySelector(select.widgets.amount.linkDecrease);
-    thisWidget.linkIncrease = thisWidget.element.querySelector(select.widgets.amount.linkIncrease);
+    thisWidget.dom.input = thisWidget.dom.wrapper.querySelector(select.widgets.amount.input);
+    thisWidget.dom.linkDecrease = thisWidget.dom.wrapper.querySelector(select.widgets.amount.linkDecrease);
+    thisWidget.dom.linkIncrease = thisWidget.dom.wrapper.querySelector(select.widgets.amount.linkIncrease);
   }
 
-  setValue(value){
+  isValid(value){
+    return !isNaN(value)
+      && value >= settings.amountWidget.defaultMin 
+      && value <= settings.amountWidget.defaultMax;
+  }
+
+  renderValue(){
     const thisWidget = this;
 
-    const newValue = parseInt(value);
-
-    /* TODO: Add validation */
-    if(thisWidget.value !== newValue && !isNaN(newValue) && newValue >= settings.amountWidget.defaultMin && newValue <= settings.amountWidget.defaultMax) {
-      thisWidget.value = newValue;
-    }
-
-    thisWidget.announce(); // śmiga mimo braku przeniesienia, mój drugi strzał to wrzucenie na koniec ifa
-
-    thisWidget.input.value = thisWidget.value;
+    thisWidget.dom.input.value = thisWidget.value;
   }
 
   initActions(){
     const thisWidget = this;
 
-    thisWidget.input.addEventListener('change', function(){ thisWidget.setValue(thisWidget.input.value); });
-    thisWidget.linkDecrease.addEventListener('click', function (){ event.preventDefault(); thisWidget.setValue(thisWidget.value - 1); });
-    thisWidget.linkIncrease.addEventListener('click', function () { event.preventDefault(); thisWidget.setValue(thisWidget.value + 1); });
-  }
+    thisWidget.dom.input.addEventListener('change', function(){ 
+      thisWidget.setValue(thisWidget.dom.input.value); 
+    });
 
-  announce(){
-    const thisWidget = this;
+    thisWidget.dom.linkDecrease.addEventListener('click', function (){ 
+      event.preventDefault(); 
+      thisWidget.setValue(thisWidget.value - 1); 
+    });
 
-    const event = new CustomEvent('updated', {bubbles: true});
-    thisWidget.element.dispatchEvent(event);
+    thisWidget.dom.linkIncrease.addEventListener('click', function (){ 
+      event.preventDefault(); 
+      thisWidget.setValue(thisWidget.value + 1); 
+    });
   }
 }
 
