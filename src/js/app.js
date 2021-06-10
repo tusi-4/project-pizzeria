86a154ee4dcab2f26d22c720a12fa143c66713a0
Marta Wojdat <marjow4@gmail.com>
2021-06-10 23:15:57 +0200

    Prepare for Heroku

--- src/js/app.js
+++ src/js/app.js
@@ -2,7 +2,6 @@ import {settings, select, classNames} from './settings.js';
 import Product from './components/Product.js';
 import Cart from './components/Cart.js';
 import Booking from './components/Booking.js';
-// import Home from './components/Home.js';
 
 const app = {
   initPages: function(){
--- src/js/settings.js
+++ src/js/settings.js
@@ -117,7 +117,7 @@ export const settings = {
     tableIdAttribute: 'data-table',
   },
   db: {
-    url: '//localhost:3131',
+    url: '//' + window.location.hostname + (window.location.hostname=='localhost' ? ':3131' : ''),
     products: 'products',
     orders: 'orders', // w kodilli order: 'order'
     bookings: 'bookings',
